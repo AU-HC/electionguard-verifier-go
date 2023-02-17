d43c42a662f17701199462a11da2ec0a5c46d7c3
AU-HC <202004086@post.au.dk>
2023-02-18 00:49:52 +0100

    Now validating step 4

--- core/arithmatic.go
+++ core/arithmatic.go
@@ -3,7 +3,34 @@ package core
 import (
 	"electionguard-verifier-go/schema"
 	"electionguard-verifier-go/utility"
+	"math/big"
 )
+
+func isValidResidue(a schema.BigInt) bool {
+	// Checking the value is in range
+	cons := utility.MakeCorrectElectionConstants()
+	p := cons.P
+	q := cons.Q
+	zero := big.NewInt(0)
+	var one schema.BigInt
+	one.SetString("1", 10)
+
+	valueIsAboveOrEqualToZero := zero.Cmp(&a.Int) <= 0
+	valueIsSmallerThanP := p.Cmp(&a.Int) == 1
+	validResidue := (powP(&a, &q)).Compare(&one)
+
+	return valueIsAboveOrEqualToZero && valueIsSmallerThanP && validResidue
+}
+
+func isInRange(a schema.BigInt) bool {
+	q := utility.MakeCorrectElectionConstants().Q.Int
+	zero := big.NewInt(0)
+
+	valueIsAboveOrEqualToZero := zero.Cmp(&a.Int) <= 0
+	valueIsSmallerThanP := q.Cmp(&a.Int) > 0
+
+	return valueIsAboveOrEqualToZero && valueIsSmallerThanP
+}
 
 func powP(b, e *schema.BigInt) *schema.BigInt {
 	var result schema.BigInt
@@ -24,6 +51,16 @@ func mulP(a, b *schema.BigInt) *schema.BigInt {
 	// Multiply the two numbers mod n
 	resultOfMultiplication := modOfA.Mul(modOfA, modOfB)
 	result.Mod(resultOfMultiplication, &p)
+
+	return &result
+}
+
+func addQ(a, b *schema.BigInt) *schema.BigInt {
+	var result schema.BigInt
+	q := utility.MakeCorrectElectionConstants().Q.Int
+
+	result.Add(&b.Int, &a.Int)
+	result.Mod(&result.Int, &q)
 
 	return &result
 }
--- core/verifier.go
+++ core/verifier.go
@@ -70,6 +70,48 @@ func (v *Verifier) Verify(path string) bool {
 	}
 
 	// Correctness of selection encryptions (Step 4)
+	selectionEncryptionValidationHelper := MakeValidationHelper(v.logger, "Correctness of selection encryptions (Step 4)")
+	for i, ballot := range args.SubmittedBallots {
+		for j, contest := range ballot.Contests {
+			for k, ballotSelection := range contest.BallotSelections {
+				a := ballotSelection.Ciphertext.Pad
+				b := ballotSelection.Ciphertext.Data
+				a0 := ballotSelection.Proof.ProofZeroPad
+				b0 := ballotSelection.Proof.ProofZeroData
+				a1 := ballotSelection.Proof.ProofOnePad
+				b1 := ballotSelection.Proof.ProofOneData
+				c := ballotSelection.Proof.Challenge
+				c0 := ballotSelection.Proof.ProofZeroChallenge
+				c1 := ballotSelection.Proof.ProofOneChallenge
+				v0 := ballotSelection.Proof.ProofZeroResponse
+				v1 := ballotSelection.Proof.ProofOneResponse
+
+				// TODO: Refactor at some point
+				selectionEncryptionValidationHelper.AddCheck("(4.A) a is in the set Z_p^r ("+strconv.Itoa(i)+","+strconv.Itoa(j)+","+strconv.Itoa(k)+")", isValidResidue(a))
+				selectionEncryptionValidationHelper.AddCheck("(4.A) b is in the set Z_p^r ("+strconv.Itoa(i)+","+strconv.Itoa(j)+","+strconv.Itoa(k)+")", isValidResidue(b))
+				selectionEncryptionValidationHelper.AddCheck("(4.A) a0 is in the set Z_p^r ("+strconv.Itoa(i)+","+strconv.Itoa(j)+","+strconv.Itoa(k)+")", isValidResidue(a0))
+				selectionEncryptionValidationHelper.AddCheck("(4.A) b0 is in the set Z_p^r ("+strconv.Itoa(i)+","+strconv.Itoa(j)+","+strconv.Itoa(k)+")", isValidResidue(b0))
+				selectionEncryptionValidationHelper.AddCheck("(4.A) a1 is in the set Z_p^r ("+strconv.Itoa(i)+","+strconv.Itoa(j)+","+strconv.Itoa(k)+")", isValidResidue(a1))
+				selectionEncryptionValidationHelper.AddCheck("(4.A) b1 is in the set Z_p^r ("+strconv.Itoa(i)+","+strconv.Itoa(j)+","+strconv.Itoa(k)+")", isValidResidue(b1))
+				selectionEncryptionValidationHelper.AddCheck("(4.B) The challenge value c is computed correctly ("+strconv.Itoa(i)+","+strconv.Itoa(j)+","+strconv.Itoa(k)+")", c.Compare(crypto.HashElems(args.CiphertextElectionRecord.CryptoExtendedBaseHash, a, b, a0, b0, a1, b1)))
+				selectionEncryptionValidationHelper.AddCheck("(4.C) c0 is in Zq for ("+strconv.Itoa(i)+","+strconv.Itoa(j)+","+strconv.Itoa(k)+")", isInRange(c0))
+				selectionEncryptionValidationHelper.AddCheck("(4.C) c1 is in Zq for ("+strconv.Itoa(i)+","+strconv.Itoa(j)+","+strconv.Itoa(k)+")", isInRange(c1))
+				selectionEncryptionValidationHelper.AddCheck("(4.C) v0 is in Zq for ("+strconv.Itoa(i)+","+strconv.Itoa(j)+","+strconv.Itoa(k)+")", isInRange(v0))
+				selectionEncryptionValidationHelper.AddCheck("(4.C) v1 is in Zq for ("+strconv.Itoa(i)+","+strconv.Itoa(j)+","+strconv.Itoa(k)+")", isInRange(v1))
+				selectionEncryptionValidationHelper.AddCheck("(4.D) The equation c=(c0+c1) mod q is satisfied ("+strconv.Itoa(i)+","+strconv.Itoa(j)+","+strconv.Itoa(k)+")", c.Compare(addQ(&c0, &c1)))
+				selectionEncryptionValidationHelper.AddCheck("(4.E) The equation g^v0=a0*a^c0 is satisfied ("+strconv.Itoa(i)+","+strconv.Itoa(j)+","+strconv.Itoa(k)+")", powP(&correctConstants.G, &v0).Compare(mulP(&a0, powP(&a, &c0))))
+				selectionEncryptionValidationHelper.AddCheck("(4.F) The equation g^v1=a1*a^c1 is satisfied ("+strconv.Itoa(i)+","+strconv.Itoa(j)+","+strconv.Itoa(k)+")", powP(&correctConstants.G, &v1).Compare(mulP(&a1, powP(&a, &c1))))
+				selectionEncryptionValidationHelper.AddCheck("(4.G) The equation K^v0=b0*b^c0 is satisfied ("+strconv.Itoa(i)+","+strconv.Itoa(j)+","+strconv.Itoa(k)+")", powP(elgamalPublicKey, &v0).Compare(mulP(&b0, powP(&b, &c0))))
+				selectionEncryptionValidationHelper.AddCheck("(4.H) The equation g^c1=b0*b^c0 is satisfied ("+strconv.Itoa(i)+","+strconv.Itoa(j)+","+strconv.Itoa(k)+")", mulP(powP(&correctConstants.G, &c1), powP(elgamalPublicKey, &v1)).Compare(mulP(&b1, powP(&b, &c1))))
+			}
+		}
+	}
+	correctnessOfSelectionsIsNotValid := !selectionEncryptionValidationHelper.Validate()
+	if correctnessOfSelectionsIsNotValid {
+		return false
+	}
+
+	// Validate adherence to vote limits (Step 5)
 	// ...
 	// ...
 
