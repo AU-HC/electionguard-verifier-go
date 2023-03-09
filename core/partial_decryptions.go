9d7510364acfdda3587ec223951331bcc2e2729d
AU-HC <202004086@post.au.dk>
2023-03-09 16:44:37 +0100

    Now verifying step 8 correctly

--- core/partial_decryptions.go
+++ core/partial_decryptions.go
@@ -20,18 +20,18 @@ func (v *Verifier) validatePartialDecryptions(er *deserialize.ElectionRecord) {
 
 			for k, share := range selection.Shares {
 				if !share.Proof.Pad.Compare(schema.MakeBigIntFromString("0", 10)) { // Comparing with zero, will need better way of determining this TODO: Fix
-					V := share.Proof.Response
+					vi := share.Proof.Response
 					c := share.Proof.Challenge
 					ai := share.Proof.Pad
 					bi := share.Proof.Data
 					m := share.Share
 
-					helper.addCheck("(8.A) The value v is in the set Zq for "+share.ObjectId+" "+strconv.Itoa(k), isInRange(V))
+					helper.addCheck("(8.A) The value v is in the set Zq for "+share.ObjectId+" "+strconv.Itoa(k), isInRange(vi))
 					helper.addCheck("(8.B) The value a is in the set Zqr for "+share.ObjectId+" "+strconv.Itoa(k), isValidResidue(share.Proof.Pad))
 					helper.addCheck("(8.B) The value b is in the set Zqr for "+share.ObjectId+" "+strconv.Itoa(k), isValidResidue(share.Proof.Data))
 					helper.addCheck("(8.C) The challenge is computed correctly "+share.ObjectId+" "+strconv.Itoa(k), c.Compare(crypto.HashElems(extendedBaseHash, A, B, ai, bi, m)))
-					helper.addCheck("(8.D) The equation is satisfied "+share.ObjectId+" "+strconv.Itoa(k), powP(v.constants.G, &V).Compare(mulP(&ai, powP(&er.Guardians[k].ElectionPublicKey, &c))))
-					helper.addCheck("(8.E) The equation is satisfied "+share.ObjectId+" "+strconv.Itoa(k), powP(&A, &V).Compare(mulP(&bi, powP(&m, &c))))
+					helper.addCheck("(8.D) The equation is satisfied "+share.ObjectId+" "+strconv.Itoa(k), powP(v.constants.G, &vi).Compare(mulP(&ai, powP(getGuardianPublicKey(share.GuardianId, er.Guardians), &c))))
+					helper.addCheck("(8.E) The equation is satisfied "+share.ObjectId+" "+strconv.Itoa(k), powP(&A, &vi).Compare(mulP(&bi, powP(&m, &c))))
 				}
 			}
 		}
